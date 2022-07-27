1bdc5df93eb114aad1819714daba9d1d89c13d8c
Romulo Motta <pukpen@gmail.com>
2022-07-27 19:40:39 -0300

    worker em 2 localhost, ribbon, eureka-server

--- hr-worker/src/main/java/com/motta/hrworker/resources/WorkerController.java
+++ hr-worker/src/main/java/com/motta/hrworker/resources/WorkerController.java
@@ -2,7 +2,10 @@ package com.motta.hrworker.resources;
 
 import com.motta.hrworker.entities.Worker;
 import com.motta.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,11 @@ import java.util.List;
 @RequestMapping(value="/workers")
 public class WorkerController {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Autowired
+    private Environment env;
+
     @Autowired
     private WorkerRepository repository;
 
@@ -26,6 +34,9 @@ public class WorkerController {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+
+        logger.info("PORT = " + env.getProperty("local.server.port"));
+
         Worker worker = repository.findById(id).get();
         return ResponseEntity.ok().body(worker);
     }
