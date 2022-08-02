3c9b842888fd65e584f458f70a3ac9dbb45d8337
Romulo Motta <pukpen@gmail.com>
2022-08-01 23:54:44 -0300

    UserRepository, UserResource, User, Api Gateway app prop

---
+++ hr-user/src/main/java/com/motta/hruser/resources/UserResource.java
@@ -0,0 +1,31 @@
+package com.motta.hruser.resources;
+
+import com.motta.hruser.entities.User;
+import com.motta.hruser.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = repository.findById(id).get();
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findByEmail(@RequestParam String email) {
+        User user = repository.findByEmail(email);
+        return ResponseEntity.ok(user);
+    }
+}
