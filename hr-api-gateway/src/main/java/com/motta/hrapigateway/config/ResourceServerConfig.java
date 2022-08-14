98e5186bd05ed695c363b85ced6bfe6b6f700eb1
Romulo Motta <pukpen@gmail.com>
2022-08-14 10:56:57 -0300

    db config server

--- hr-api-gateway/src/main/java/com/motta/hrapigateway/config/ResourceServerConfig.java
+++ hr-api-gateway/src/main/java/com/motta/hrapigateway/config/ResourceServerConfig.java
@@ -56,7 +56,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowedOrigins(Arrays.asList("*"));
-        corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+        corsConfig.setAllowedMethods(Arrays.asList(
+                "POST", "GET", "PUT", "DELETE", "PATCH"));
         corsConfig.setAllowCredentials(true);
         corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
 
